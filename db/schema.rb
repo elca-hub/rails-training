f1085a3f792851adc5d3fef3b614849aaf57a38d
elca-hub <csubaru07@gmail.com>
2023-09-12 17:12:38 +0900

    subjectsのカラム全てにNOT NULL制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_12_064447) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_12_081017) do
   create_table "subjects", charset: "utf8mb4", force: :cascade do |t|
-    t.string "name"
-    t.string "evaluation"
-    t.integer "unit_value"
+    t.string "name", null: false
+    t.string "evaluation", null: false
+    t.integer "unit_value", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
